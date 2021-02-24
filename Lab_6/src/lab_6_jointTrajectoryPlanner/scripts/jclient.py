c51ac4b0d90770bc522bd678433431fd7948e57a
bllgg <gunathilakebllg@gmail.com>
2021-02-24 17:28:31 +0530

    Lab 6 completed

---
+++ Lab_6/src/lab_6_jointTrajectoryPlanner/scripts/jclient.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import sys
+import rospy
+import time
+
+from open_manipulator_msgs.srv import *
+from open_manipulator_msgs.msg import *
+
+def move_in_joint_space(joint1, joint2, joint3, joint4, path_time):
+
+    rospy.wait_for_service('goal_joint_space_path_custom')
+
+    try:
+        setJointPath = rospy.ServiceProxy('goal_joint_space_path_custom', SetJointPosition)
+
+        request =  SetJointPositionRequest()
+
+        request.joint_position.joint_name = ["joint1", "joint2", "joint3", "joint4"]
+        request.joint_position.position = [joint1, joint2, joint3, joint4]
+        request.path_time = path_time
+
+        response = setJointPath(request)
+
+        return response.is_planned
+
+    except rospy.ServiceException as e:
+
+        print("Service call failed: %s"%e)
+
+def activate_actuators(state):
+
+    rospy.wait_for_service('set_actuator_state')
+
+    try:
+
+        activate = rospy.ServiceProxy('set_actuator_state', SetActuatorState)
+        
+        srv = SetActuatorStateRequest()
+
+        srv.set_actuator_state = state
+
+        response =  activate(srv)
+
+        return response.is_planned
+
+    except rospy.ServiceException as e:
+
+        print("Service call failed: %s"%e)
+
+if __name__ == "__main__":
+
+    joint_space = [[0.0, 0.0, 0.0, 0.0, 5.0], [-0.2, 0.0, 0.0, 0.0, 5.0], [-0.2, 0.4, 0.0, 0.0, 5.0], [0.2, -0.2, -0.4, 0.0, 5.0], [-0.4, 0.2, 0.2, 0.2, 5.0]]
+
+
+    if activate_actuators(True):
+        print("Activation completed")
+    else:
+        print("Activation failed")
+
+    for joint in joint_space :
+
+        if move_in_joint_space(joint[0], joint[1], joint[2], joint[3], joint[4]):
+            print("Successfully moved")
+        else:
+            print("Failed to move")
+
+        time.sleep(6)
+
+    # if move_in_joint_space(joint_space[itr][0], joint_space[itr][1], joint_space[itr][2], joint_space[itr][3], joint_space[itr][4]):
+    #     print("Successfully moved")
+    # else:
+    #     print("Failed to move")
+
+    # time.sleep(6.0)
+
+    if activate_actuators(False):
+        print("Deactivation completed")
+    else:
+        print("Deactivation failed")
+
+
+    # if activate_actuators(True):
+    #     print("Activation completed")
+    # else:
+    #     print("Activation failed")
+
+    # if move_in_joint_space(-0.4, 0.2, 0.2, 0.2, 5.0):
+    #     print("Successfully moved")
+    # else:
+    #     print("Failed to move")
+
+    # time.sleep(6.0)
+
+    # if activate_actuators(False):
+    #     print("Deactivation completed")
+    # else:
+    #     print("Deactivation failed")
